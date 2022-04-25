2f93576f57a09f9d38f28d96f10f79b219c7481a
wangkun <wangkun@woodemi.com>
2022-04-25 22:10:26 +0800

    Add Windows for quick_blue
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ packages/quick_blue/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <quick_blue/quick_blue_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  QuickBluePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("QuickBluePlugin"));
+}
---
+++ packages/quick_blue/windows/quick_blue_plugin.cpp
@@ -0,0 +1,82 @@
+#include "include/quick_blue/quick_blue_plugin.h"
+
+// This must be included before many other Windows headers.
+#include <windows.h>
+
+// For getPlatformVersion; remove unless needed for your plugin implementation.
+#include <VersionHelpers.h>
+
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar_windows.h>
+#include <flutter/standard_method_codec.h>
+
+#include <map>
+#include <memory>
+#include <sstream>
+
+namespace {
+
+class QuickBluePlugin : public flutter::Plugin {
+ public:
+  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+
+  QuickBluePlugin();
+
+  virtual ~QuickBluePlugin();
+
+ private:
+  // Called when a method is called on this plugin's channel from Dart.
+  void HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+};
+
+// static
+void QuickBluePlugin::RegisterWithRegistrar(
+    flutter::PluginRegistrarWindows *registrar) {
+  auto channel =
+      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+          registrar->messenger(), "quick_blue",
+          &flutter::StandardMethodCodec::GetInstance());
+
+  auto plugin = std::make_unique<QuickBluePlugin>();
+
+  channel->SetMethodCallHandler(
+      [plugin_pointer = plugin.get()](const auto &call, auto result) {
+        plugin_pointer->HandleMethodCall(call, std::move(result));
+      });
+
+  registrar->AddPlugin(std::move(plugin));
+}
+
+QuickBluePlugin::QuickBluePlugin() {}
+
+QuickBluePlugin::~QuickBluePlugin() {}
+
+void QuickBluePlugin::HandleMethodCall(
+    const flutter::MethodCall<flutter::EncodableValue> &method_call,
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+  if (method_call.method_name().compare("getPlatformVersion") == 0) {
+    std::ostringstream version_stream;
+    version_stream << "Windows ";
+    if (IsWindows10OrGreater()) {
+      version_stream << "10+";
+    } else if (IsWindows8OrGreater()) {
+      version_stream << "8";
+    } else if (IsWindows7OrGreater()) {
+      version_stream << "7";
+    }
+    result->Success(flutter::EncodableValue(version_stream.str()));
+  } else {
+    result->NotImplemented();
+  }
+}
+
+}  // namespace
+
+void QuickBluePluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+  QuickBluePlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarManager::GetInstance()
+          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+}
