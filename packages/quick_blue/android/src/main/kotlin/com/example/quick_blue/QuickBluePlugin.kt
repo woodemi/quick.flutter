b9ddc018a3d897fb786f80631f6ae90c713a16ef
wangkun <wangkun@woodemi.com>
2022-04-26 17:01:08 +0800

    Impl on Android
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- packages/quick_blue/android/src/main/kotlin/com/example/quick_blue/QuickBluePlugin.kt
+++ packages/quick_blue/android/src/main/kotlin/com/example/quick_blue/QuickBluePlugin.kt
@@ -1,35 +1,125 @@
 package com.example.quick_blue
 
+import android.annotation.SuppressLint
+import android.bluetooth.*
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.*
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+private const val TAG = "QuickBluePlugin"
 
 /** QuickBluePlugin */
-class QuickBluePlugin: FlutterPlugin, MethodCallHandler {
+@SuppressLint("MissingPermission")
+class QuickBluePlugin: FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  private lateinit var method : MethodChannel
+  private lateinit var eventScanResult : EventChannel
+  private lateinit var messageConnector: BasicMessageChannel<Any>
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "quick_blue")
-    channel.setMethodCallHandler(this)
-  }
+    method = MethodChannel(flutterPluginBinding.binaryMessenger, "quick_blue/method")
+    eventScanResult = EventChannel(flutterPluginBinding.binaryMessenger, "quick_blue/event.scanResult")
+    messageConnector = BasicMessageChannel(flutterPluginBinding.binaryMessenger, "quick_blue/message.connector", StandardMessageCodec.INSTANCE)
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
+    method.setMethodCallHandler(this)
+    eventScanResult.setStreamHandler(this)
+
+    context = flutterPluginBinding.applicationContext
+    mainThreadHandler = Handler(Looper.getMainLooper())
+    bluetoothManager = flutterPluginBinding.applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    bluetoothManager.adapter.bluetoothLeScanner?.stopScan(scanCallback)
+
+    eventScanResult.setStreamHandler(null)
+    method.setMethodCallHandler(null)
+  }
+
+  private lateinit var context: Context
+  private lateinit var mainThreadHandler: Handler
+  private lateinit var bluetoothManager: BluetoothManager
+
+  private fun sendMessage(messageChannel: BasicMessageChannel<Any>, message: Map<String, Any>) {
+    mainThreadHandler.post { messageChannel.send(message) }
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    when (call.method) {
+      "isBluetoothAvailable" -> {
+        result.success(bluetoothManager.adapter.isEnabled)
+      }
+      "startScan" -> {
+        bluetoothManager.adapter.bluetoothLeScanner?.startScan(scanCallback)
+        result.success(null)
+      }
+      "stopScan" -> {
+        bluetoothManager.adapter.bluetoothLeScanner?.stopScan(scanCallback)
+        result.success(null)
+      }
+      else -> {
+        result.notImplemented()
+      }
+    }
+  }
+
+  private val scanCallback = object : ScanCallback() {
+    override fun onScanFailed(errorCode: Int) {
+      Log.v(TAG, "onScanFailed: $errorCode")
+    }
+
+    override fun onScanResult(callbackType: Int, result: ScanResult) {
+      Log.v(TAG, "onScanResult: $callbackType + $result")
+      scanResultSink?.success(mapOf<String, Any>(
+              "name" to (result.device.name ?: ""),
+              "deviceId" to result.device.address,
+              "manufacturerDataHead" to (result.manufacturerDataHead ?: byteArrayOf()),
+              "rssi" to result.rssi
+      ))
+    }
+
+    override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+      Log.v(TAG, "onBatchScanResults: $results")
+    }
+  }
+
+  private var scanResultSink: EventChannel.EventSink? = null
+
+  override fun onListen(args: Any?, eventSink: EventChannel.EventSink?) {
+    val map = args as? Map<String, Any> ?: return
+    when (map["name"]) {
+      "scanResult" -> scanResultSink = eventSink
+    }
+  }
+
+  override fun onCancel(args: Any?) {
+    val map = args as? Map<String, Any> ?: return
+    when (map["name"]) {
+      "scanResult" -> scanResultSink = null
+    }
   }
 }
+
+val ScanResult.manufacturerDataHead: ByteArray?
+  get() {
+    val sparseArray = scanRecord?.manufacturerSpecificData ?: return null
+    if (sparseArray.size() == 0) return null
+
+    return sparseArray.keyAt(0).toShort().toByteArray() + sparseArray.valueAt(0)
+  }
+
+fun Short.toByteArray(byteOrder: ByteOrder = ByteOrder.LITTLE_ENDIAN): ByteArray =
+        ByteBuffer.allocate(2 /*Short.SIZE_BYTES*/).order(byteOrder).putShort(this).array()
