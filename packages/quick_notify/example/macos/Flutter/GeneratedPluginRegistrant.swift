55cda6951592175f26871dcebefb4543b2fbf180
wangkun <wangkun@woodemi.com>
2022-06-02 16:40:03 +0800

    Add macOS for quick_notify
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ packages/quick_notify/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import quick_notify
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  QuickNotifyPlugin.register(with: registry.registrar(forPlugin: "QuickNotifyPlugin"))
+}
