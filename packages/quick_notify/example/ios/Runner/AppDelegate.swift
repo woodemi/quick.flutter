1dc74cd22604a220725e222b5b5575ce394a4d48
wangkun <wangkun@woodemi.com>
2022-06-03 08:54:20 +0800

    Impl on iOS
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- packages/quick_notify/example/ios/Runner/AppDelegate.swift
+++ packages/quick_notify/example/ios/Runner/AppDelegate.swift
@@ -8,6 +8,20 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    UNUserNotificationCenter.current().delegate = self
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { (grantd, error) in
+        guard error == nil else {
+            print("requestAuthorization error \(error)")
+            return
+        }
+        print("requestAuthorization granted \(grantd)")
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+extension AppDelegate {
+    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler(.alert)
+    }
 }
--- packages/quick_notify/ios/Classes/SwiftQuickNotifyPlugin.swift
+++ packages/quick_notify/ios/Classes/SwiftQuickNotifyPlugin.swift
@@ -9,6 +9,32 @@ public class SwiftQuickNotifyPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "hasPermission":
+      UNUserNotificationCenter.current().getNotificationSettings { settings in
+        result(settings.authorizationStatus != .denied)
+      }
+    case "requestPermission":
+      UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+        result(granted)
+      }
+    case "notify":
+      let args = call.arguments as! Dictionary<String, Any>
+      let title = args["title"] as! String
+      let content = args["content"] as! String
+
+      let notification = UNMutableNotificationContent()
+      notification.title = title
+      notification.body = content
+      let request = UNNotificationRequest(identifier: "quick_notify", content: notification, trigger: nil)
+      UNUserNotificationCenter.current().add(request) { error in
+        if (error != nil) {
+          print("quick_notify error: \(error)")
+        }
+      }
+      result(nil)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
   }
 }
