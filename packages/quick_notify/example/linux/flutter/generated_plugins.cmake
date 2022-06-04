29ec50b34ab46df8c9ddd22bb3d43d6926ff26b3
wangkun <wangkun@woodemi.com>
2022-06-04 11:40:45 +0800

    Refactor QuickNotifyLinux with pure Dart package
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- packages/quick_notify/example/linux/flutter/generated_plugins.cmake
+++ packages/quick_notify/example/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  quick_notify
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
---
+++ packages/quick_notify/lib/src/quick_notify_linux.dart
@@ -0,0 +1,32 @@
+import 'package:desktop_notifications/desktop_notifications.dart';
+
+import 'quick_notify_platform_interface.dart';
+
+class QuickNotifyLinux extends QuickNotifyPlatform {
+  // For example/.dart_tool/flutter_build/generated_main.dart
+  static registerWith() {
+    QuickNotifyPlatform.instance = QuickNotifyLinux();
+  }
+
+  @override
+  Future<bool> hasPermission() async {
+    return true;
+  }
+
+  @override
+  Future<bool> requestPermission() async {
+    return true;
+  }
+
+  @override
+  void notify({
+    required String title,
+    String? content,
+  }) {
+    () async {
+      var client = NotificationsClient();
+      await client.notify(title, body: content ?? '');
+      await client.close();
+    }();
+  }
+}
