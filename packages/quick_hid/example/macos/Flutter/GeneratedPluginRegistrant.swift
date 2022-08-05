a4e402d8f1bba0075383d439c7b97f4227224fee
wangkun <wangkun@woodemi.com>
2022-08-05 15:50:21 +0800

    Add macOS for quick_hid
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ packages/quick_hid/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import quick_hid
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  QuickHidPlugin.register(with: registry.registrar(forPlugin: "QuickHidPlugin"))
+}
