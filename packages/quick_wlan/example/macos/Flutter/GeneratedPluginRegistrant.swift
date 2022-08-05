fbce0b74b9e413c14ac7ed2a9d56cbc5fdcc5bf0
wangkun <wangkun@woodemi.com>
2022-08-05 19:14:35 +0800

    Add macOS for quick_wlan
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ packages/quick_wlan/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import quick_wlan
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  QuickWlanPlugin.register(with: registry.registrar(forPlugin: "QuickWlanPlugin"))
+}
