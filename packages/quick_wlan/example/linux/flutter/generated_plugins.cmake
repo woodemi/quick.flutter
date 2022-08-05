cc75635be8cd08c05762c01f484532804741e2b4
wangkun <wangkun@woodemi.com>
2022-08-05 19:22:17 +0800

    Add Linux for quick_wlan
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- packages/quick_wlan/example/linux/flutter/generated_plugins.cmake
+++ packages/quick_wlan/example/linux/flutter/generated_plugins.cmake
@@ -3,13 +3,12 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  quick_wlan
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
 
 foreach(plugin ${FLUTTER_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows plugins/${plugin})
+  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/linux plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
--- packages/quick_wlan/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/quick_wlan/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <quick_wlan/quick_wlan_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  QuickWlanPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("QuickWlanPlugin"));
 }
